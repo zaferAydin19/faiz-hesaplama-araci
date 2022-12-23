720929f8e67b03092a1fdfd9f4beb688e7bca425
Zafer Aydın <zaferaydin93@outlook.com>
2022-12-23 22:54:28 +0300

    "

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const displayMessage = function(message){
     document.querySelector('.message').textContent = message;
 }
+ 
 
  const getSelectValue = function () {
     var selectedValue = document.getElementById('list').value;
     var selectedVade = parseInt(selectedValue);
-    console.log(selectedVade, typeof selectedVade);
+    // console.log(selectedVade, typeof selectedVade);
     return selectedVade;
 }
 getSelectValue();
@@ -16,28 +17,44 @@ document.querySelector('.hesapla').addEventListener('click', function(){
     let faizOrani = Number(document.querySelector('.faiz-orani').value);
     let aylikFaiz = anaPara  * faizOrani  * (getSelectValue() / 36500);
     let totalMoney = aylikFaiz + anaPara; 
-    console.log(anaPara, typeof anaPara, faizOrani, typeof faizOrani, aylikFaiz);
-    
 
+    let aylikKazanc = function(){ 
+        if(getSelectValue() == 92){
+            document.querySelector('.mountly-earn').textContent = (aylikFaiz / 3 ).toFixed(2);
+        } else if(getSelectValue() == 181){
+            document.querySelector('.mountly-earn').textContent = (aylikFaiz / 6 ).toFixed(2);
+        } else if(getSelectValue() == 360){
+            document.querySelector('.mountly-earn').textContent = (aylikFaiz / 12).toFixed(2);
+        } else {
+            document.querySelector('.mountly-earn').textContent = '';
+        }
+    };
+
+    aylikKazanc();
+
+    console.log(anaPara, typeof anaPara, faizOrani, typeof faizOrani, aylikFaiz,aylikKazanc());
+   
     if(!anaPara) {
         document.querySelector('.message').innerHTML = "Lütfen <b>Ana Para</b> Miktarı Belirtiniz!"
     } else if (!faizOrani){
         document.querySelector('.message').innerHTML = "Lütfen <b>Faiz Oranı</b> Miktarı Belirtiniz!"
     } else {
         displayMessage('');
-        document.querySelector('.mountly-earn').textContent = '₺'+ aylikFaiz.toFixed(2);
+        document.querySelector('.faiz-earn').textContent = '₺'+ aylikFaiz.toFixed(2);
         document.querySelector('.total-money').textContent = '₺'+totalMoney.toFixed(2);
+        // document.querySelector('.mountly-earn').textContent = '₺'+aylikKazanc.toFixed(2);
     }
 
     document.querySelector('.temizle').addEventListener('click', function(){
 
         document.querySelector('.anapara').value = '';
         document.querySelector('.faiz-orani').value = '';
-        document.querySelector('.mountly-earn').textContent = '';
+        document.querySelector('.faiz-earn').textContent = '';
         document.querySelector('.total-money').textContent = '';
+        document.querySelector('.mountly-earn').textContent = '';
         displayMessage('');
+        getSelectValue();
         console.log("Temizle'ye basıldı!");
     
     });
-
 });
