8f5562776662b57924bb58ac401c9393b3a7e481
zaferaydin <zaferaydin93@outlook.com>
2023-01-16 18:13:39 +0300

    Resultlara virgül eklendi

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const displayMessage = function(message){
     document.querySelector('.message').textContent = message;
 }
+
+function format(input)
+            {
+                var nStr = input.value + '';
+                nStr = nStr.replace( /\,/g, "");
+                x = nStr.split( '.' );
+                x1 = x[0];
+                x2 = x.length > 1 ? '.' + x[1] : '';
+                var rgx = /(\d+)(\d{3})/;
+                while ( rgx.test(x1) ) {
+                    x1 = x1.replace( rgx, '$1' + ',' + '$2' );
+                }
+                input.value = x1 + x2;
+            }
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
  
  const getSelectValue = function () {
     var selectedValue = document.getElementById('list').value;
@@ -19,11 +37,11 @@ document.querySelector('.hesapla').addEventListener('click', function(){
 
     let aylikKazanc = function(){ 
         if(getSelectValue() == 92){
-            document.querySelector('.mountly-earn').textContent = (aylikFaiz / 3 ).toFixed(2);
+            document.querySelector('.mountly-earn').textContent = numberWithCommas((aylikFaiz/3).toFixed(2));
         } else if(getSelectValue() == 181){
-            document.querySelector('.mountly-earn').textContent = (aylikFaiz / 6 ).toFixed(2);
+            document.querySelector('.mountly-earn').textContent = numberWithCommas((aylikFaiz/6).toFixed(2));
         } else if(getSelectValue() == 360){
-            document.querySelector('.mountly-earn').textContent = (aylikFaiz / 12).toFixed(2);
+            document.querySelector('.mountly-earn').textContent = numberWithCommas((aylikFaiz/12).toFixed(2));
         } else {
             document.querySelector('.mountly-earn').textContent = '';
         }
@@ -39,8 +57,8 @@ document.querySelector('.hesapla').addEventListener('click', function(){
         document.querySelector('.message').innerHTML = "Lütfen <b>Faiz Oranı</b> Miktarı Belirtiniz!"
     } else {
         displayMessage('');
-        document.querySelector('.faiz-earn').textContent = '₺'+ aylikFaiz.toFixed(2);
-        document.querySelector('.total-money').textContent = '₺'+totalMoney.toFixed(2);
+        document.querySelector('.faiz-earn').textContent = '₺'+ numberWithCommas(aylikFaiz.toFixed(2));
+        document.querySelector('.total-money').textContent = '₺'+ numberWithCommas(totalMoney.toFixed(2));
         // document.querySelector('.mountly-earn').textContent = '₺'+aylikKazanc.toFixed(2);
     }
 
